a0f695afa2884144c9add36f6584bf4c724d4337
AndikhaW <andikha.wisanggeni@ui.ac.id>
2022-10-31 22:57:49 +0700

    TP6_Andikha Wisanggeni

--- src/AndikhaWisanggeniJSleepRJ/JSleep.java
+++ src/AndikhaWisanggeniJSleepRJ/JSleep.java
@@ -3,21 +3,46 @@ package AndikhaWisanggeniJSleepRJ;
 //import java.util.Calendar;
 import java.sql.*;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
 
 public class JSleep
 {
+
     public static Room createRoom(){
         Price price = new Price (100000, 5);
         Room room = new Room ("hotel", 30, price, Facility.AC,City.DEPOK,"Jl. Margonda Raya");
         return room;
     }
+    class Country{
+        public String name;
+        public int population;
+        public List<String> listOfStates;
+    }
     public static void main (String[] args){
+        String filepath = "C:\\Users\\Andikha Wisanggeni\\OneDrive\\Documents\\Kuliah\\Semester 3\\OOP\\Code Praktikum\\city.json";
+        Gson gson = new Gson();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            Country input = gson.fromJson(br, Country.class);
+            System.out.println("name: " + input.name);
+            System.out.println("population: " + input.population);
+            System.out.println("states: ");
+            input.listOfStates.forEach(state -> System.out.println(state));
 
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        /*
         ArrayList<Room> RoomSerialized = new ArrayList<>();
         for(int i = 0; i < 5; i++){
             RoomSerialized.add(i, JSleep.createRoom());
             System.out.println(RoomSerialized.get(i).toString());
-        }
+        }*/
         /*
         Room RoomA = JSleep.createRoom();
         Room RoomB = JSleep.createRoom();
